881c0bb205811a65aa55d0dca0ac63ac7bab8aa8
Usuario <Usuario@Usuario-PC>
2022-06-09 15:07:47 -0500

    Taller 02, Mónica Mendoza

---
+++ cv1_ejercicio/src/cv1_ejercicio/Cv1_ejercicio.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cv1_ejercicio;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Cv1_ejercicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("V0");
+    }
+    
+}
